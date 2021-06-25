614fc610089f5e07ef81cd62b088563c2bfe55c5
hmonzon1 <orlandomonzon96@hotmail.com>
2021-06-24 20:26:54 -0600

    Cambio poemas

---
+++ MyAppListado/app/src/main/java/com/hmonzon/myapplistado/MAVocales.java
@@ -0,0 +1,14 @@
+package com.hmonzon.myapplistado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MAVocales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mavocales);
+    }
+}
